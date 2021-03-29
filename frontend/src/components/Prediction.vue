bfb05a8a88939d7cadfa7d9e21933c7991ba47d6
Tomas <cernytomas@hotmail.com>
2021-03-29 12:55:09 +0200

    setup_vue

---
+++ frontend/src/components/Prediction.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3>Predicting spotify popularity</h3>
+</template>
+
+<script>
+export default {
+    methods:{
+        async predict(){
+            let values = {
+                artists: 6316.0,
+                acousticness : 0.678,
+                year: 2020 
+            }
+
+            let res = await fetch('api/predict', {
+                method: 'POST',
+                body: JSON.stringify(values)
+            })
+
+            let prediction = await res.json()
+
+            console.log(prediction)
+        }
+    },
+
+    created(){
+        this.predict()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
