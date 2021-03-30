8d067ab650f4cbec32d6bfbd5c4419299fd8c1b6
Martina <a.martina.eriksson@hotmail.com>
2021-03-30 22:42:37 +0200

    Added bootstrap again
    Seems to work better now.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <Prediction />
+  <p>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <Prediction />
+  </p>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 createApp(App).mount('#app')
