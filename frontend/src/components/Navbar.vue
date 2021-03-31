78f09182ed5ad81d144d625d3bee572bed3b8613
Martina <a.martina.eriksson@hotmail.com>
2021-03-31 12:46:10 +0200

    added navbar
    needs fixing

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="page-header">
+      <h1>My First Bootstrap Page</h1>
+      <p>Resize this responsive page to see the effect!</p>
+      <!-- <nav>
+          <router-link to='/'>Home</router-link>
+          <router-link to="/predict">Predict</router-link>
+          <router-link to='/statistics'>Statistics</router-link>
+      </nav> -->
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
