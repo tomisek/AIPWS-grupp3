8e103ba4b98a4741f45403d65da9d51da5e81faf
Tomas <cernytomas@hotmail.com>
2021-03-31 08:49:42 +0200

    vue vuex

--- frontend/src/components/Prediction.vue
+++ frontend/src/components/Prediction.vue
@@ -1,15 +1,33 @@
 <template>
-  <h3>Predicting spotify popularity</h3>
+    <form @submit.prevent ="predict">
+      <h3>Predicting spotify popularity</h3>
+      <input v-model="artists" type="range" name="artist" placeholder="artist" min="1" max="30000" >
+      <label for="artist">Artist</label><br>
+      <input v-model="acousticness" type="range" name="acousticness" placeholder="acousticness" min="0" max="1"  step="0.001">
+      <label for="acousticness">Acousticness</label><br>
+      <input v-model="year" type="range" name="year" placeholder="year" min="1920" max="2021" >
+      <label for="year">Year</label><br>
+      <button>test</button>
+  </form>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            artists: 15000,
+            acousticness: 0.5,
+            year: 1970
+        }
+    },
     methods:{
         async predict(){
+            
+            
             let values = {
-                artists: 6316.0,
-                acousticness : 0.678,
-                year: 2020 
+                artists: this.artists,
+                acousticness : this.acousticness,
+                year: this.year
             }
 
             let res = await fetch('api/predict', {
@@ -19,12 +37,14 @@ export default {
 
             let prediction = await res.json()
 
+            this.$store.commit('setPrediction', prediction)
+
             console.log(prediction)
         }
     },
 
     created(){
-        this.predict()
+    
     }
 }
 </script>
