54652a93f273591199ed98291c4baf9579656698
Martina <a.martina.eriksson@hotmail.com>
2021-03-31 10:39:23 +0200

    Added routing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import store from './store.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
 
 const app = createApp(App)
-app.use(store)
+
+app.use(router)
+
 app.mount('#app')
