579034385bc9cb4fa03821fff381515b9942d4bc
konny <konstantinmoraidis@gmail.com>
2021-04-12 01:50:49 +0200

    dropdown finally "fixed"

--- frontend/src/views/Artists.vue
+++ frontend/src/views/Artists.vue
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col">
       
-      <input @keyup.prevent ="getListOfArtists" v-model="artist" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Enter name of artist here ..." autocomplete="off">
+      <input v-on:keydown ="getListOfArtists" v-model="artist" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Enter name of artist here ..." autocomplete="off">
       <datalist id="datalistOptions">
       </datalist>
 
@@ -46,18 +46,51 @@ export default {
     },
     methods:{
 
-        delete_virtual_doms() {
-          // Iterate over the dom elts and do the
-          $("#datalistOptions").empty()
-          
-        },
-
         async getListOfArtists(event){
-            this.delete_virtual_doms()
+          // Commentary:
+          // At first this function was triggered by a keyup event. This lead to an extremely annoying flickering due to the
+          // virtual DOM that was reusing previous searches to optmize speed. When the key was pressed down one could only
+          // see the saved searches, if even for a short while. It looked like a bug in the app. I tried to find a way to override
+          // this particular optimization, but I did not manage to get anywhere. Since the keydown event was causing all problems
+          // I then choose it as the trigger for the function. Problem was that then I didn't have instant access to the latest 
+          // added character in the input. So I had to do some uggly hacking. In the end you cannot use the mouse pointer to position 
+          // yourself in the middle of the word if you want correct dropdown results. I choose to save this version simply because I 
+          // put a lot of time on this and it is really a question of what you rather not have. I hate flickering.
+          // I REALLY hope there is a smoother and a correct way to do this. /Konstantin
+            
+            $("#datalistOptions").empty()
             let artist = this.artist
 
-            
+            // Unless we're using the delete key, we're assuming that the inserted
+            // key should be at the end of the word.
+            // No need to access the latest key value if it is any of these three:
+            if (event.key !== 'Control' && event.key !== 'Alt' && event.which !== 32) {
+              artist = artist + event.key
+            }
+            // Can only use delete when positioned at the start.
+            else if (event.key == 'Delete') {
+              artist = event.key + artist  
+            }
+
+            // The space bar key (code 32) seems to be trimmed of when send with fetch function below,
+            // so we do the following hack:
+            artist = (event.which == 32 ? artist + "%20": artist) 
+
+            // Code to make backspace and delete possible. 
+            // Can only use backspace when positioned at the end.
+            // Can only use delete when positioned at the start.
+            const regex1 = /\s\SBackspace/ig;
+            artist = artist.replaceAll(regex1,"%20")
+            const regex2 = /.?Backspace/ig;
+            artist = artist.replaceAll(regex2,"")
+            const regex3 = /Delete.?/ig;
+            artist = artist.replaceAll(regex3,"")
+            const regex4 = /Delete\S\s/ig;
+            artist = artist.replaceAll(regex4,"%20")
+
+
             let res = await fetch(`/api/artists/${artist}`)
+
 
             let listOfArtists = await res.json()
             let artistList = []
@@ -67,9 +100,11 @@ export default {
 
             
 
-
+            // Ensure there will be no adding to a dropdown when the input is empty or when
+            // an artist has been selected. When you click on an option, then apperantly it
+            // counts as a key-event and the value is null.
             if (artist == '' || event.which == null) {
-              return;
+              return
             }
           
             for (let ar of artistList) {
